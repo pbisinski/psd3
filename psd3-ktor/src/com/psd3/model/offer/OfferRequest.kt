58a7a88568e792c2408924276f9fdfbf2c5640a8
Przemek Bisiński <38438763+pbisinski@users.noreply.github.com>
2019-12-15 15:35:25 +0100

    [KTOR] Dodanie nowych pol w tabeli ofert, mapowanie daty do stringa (#1)

--- psd3-ktor/src/com/psd3/model/offer/OfferRequest.kt
+++ psd3-ktor/src/com/psd3/model/offer/OfferRequest.kt
@@ -2,5 +2,8 @@ package com.psd3.model.offer
 
 class OfferRequest(
     val name: String?,
-    val description: String?
+    val ownerId: Long?,
+    val description: String?,
+    val location: String?,
+    val category: Int?
 )
--- psd3-ktor/src/com/psd3/model/offer/Offers.kt
+++ psd3-ktor/src/com/psd3/model/offer/Offers.kt
@@ -1,10 +1,39 @@
 package com.psd3.model.offer
 
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 
 object Offers : Table() {
     val id = long("id").autoIncrement().primaryKey()
+    val ownerId = long("ownerId")
     val name = varchar("name", 60)
     val description = varchar("description", 255)
-    val dateCreated = datetime("dateCreated")
+    val dateCreated = varchar("dateCreated", 100)
+    val location = varchar("location", 100)
+    val category = integer("category")
+}
+
+data class Offer(
+    val id: Long,
+    val ownerId: Long,
+    val name: String,
+    val description: String,
+    val dateCreated: String,
+    val location: String,
+    val category: Int
+) {
+
+    companion object {
+        fun fromRow(row: ResultRow): Offer = with(Offers) {
+            Offer(
+                id = row[id],
+                ownerId = row[ownerId],
+                name = row[name],
+                description = row[description],
+                dateCreated = row[dateCreated],
+                location = row[location],
+                category = row[category]
+            )
+        }
+    }
 }
